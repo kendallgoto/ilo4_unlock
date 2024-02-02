438042d655e83eb109811bf557727521bdebae62
Charleszard <deanmodpc@gmail.com>
2024-02-01 23:48:55 -0600

    Update quietFansDL380G9.sh
    Adds a fix for the DL380 Gen9 so the fans aren't maxxed out when using a graphics card.

--- scripts/quietFansDL380G9.sh
+++ scripts/quietFansDL380G9.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# I use this on my DL380p Gen8 running Proxmox VE 7.2.
-# The server has two of its eight SFF bays populated and is otherwise stock.
-# I consider this a safer solution than explicitly disabling sensors or
-# throttling fans because it reduces the minimum fan speed for an overreactive
-# sensor while keeping all other fan curves. I wouldn't say this silences the
-# server, but it does make it magnitudes quieter. Referenced from
-# https://www.reddit.com/r/homelab/comments/di3vrk/silence_of_the_fans_controlling_hp_server_fans/firx6op/
+# Used to solve the "Max fans because we decided to use a graphics card" bug. 
+# Seems that just restarting using these commands is enough to return the fan behavior to normal. 
+# Tested on DL380 Gen9
 
 # Replace these values with those relevant to your iLO. Alternatively,
 # consider defining them with environment variables.
@@ -20,5 +16,3 @@ sshpass -p $PWD ssh -oKexAlgorithms=+diffie-hellman-group14-sha1,diffie-hellman-
 
 
 # The script can be triggered on startup with a crontab.
-# While writing the cron job, a one liner can be used instead:
-# @reboot sshpass -p password ssh -oKexAlgorithms=+diffie-hellman-group14-sha1 Administrator@x.x.x.x 'fan pid 47 lo 1600'
